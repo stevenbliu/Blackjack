77b54560df6d7d927d63e384470d2b22e8c0d849
stevenbliu <steventheliu@gmail.com>
2025-07-19 15:06:57 -0700

    updated draggable cards to send events

--- frontend/src/features/websocket/actionTypes.ts
+++ frontend/src/features/websocket/actionTypes.ts
@@ -10,4 +10,7 @@ export const WS_CHAT_MESSAGE_RECEIVED = 'chat/WS_CHAT_MESSAGE_RECEIVED';
 
 export const REQUEST_PLAYER_ID = 'REQUEST_PLAYER_ID';
 export const PLAYER_ID_RECEIVED = 'PLAYER_ID_RECEIVED';
+
 export const WS_GAME_CREATED = 'WS_GAME_CREATED';
+
+export const SEND_CARD_DRAG = 'tabletop/sendCardDrag';
