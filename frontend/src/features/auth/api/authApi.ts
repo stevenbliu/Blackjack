81f01dd5052d10e5b0b05fd52d07bd0800333241
stevenbliu <steventheliu@gmail.com>
2025-12-01 10:22:30 -0800

    updated frontend pathing to auth

--- frontend/src/features/auth/api/authApi.ts
+++ frontend/src/features/auth/api/authApi.ts
@@ -14,10 +14,13 @@ interface RegisterCredentials {
 //   ? import.meta.env.VITE_DEVELOPMENT_API_URL
 //   : import.meta.env.VITE_PRODUCTION_API_URL;
 
+// const API_URL = 'http://localhost:8000'
+const API_URL = ''
+
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:8000/api/auth',
+    baseUrl: `${API_URL}/api/auth`,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
